edafdf7df8bf352a3d487bf83b554d4aa60a1130
clément mezerette <clement.mezerette@gmail.com>
2016-12-15 22:05:33 +0100

    V5

--- app/src/main/java/fr/wildcodeschool/team/thearray/Dragporte.java
+++ app/src/main/java/fr/wildcodeschool/team/thearray/Dragporte.java
@@ -1,8 +1,10 @@
 package fr.wildcodeschool.team.thearray;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.jmedeisis.draglinearlayout.DragLinearLayout;
 
@@ -19,5 +21,17 @@ public class Dragporte extends AppCompatActivity {
             View child = dragLinearLayout.getChildAt(i);
             dragLinearLayout.setViewDraggable(child, child);
         }
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View V){
+                {
+                    Intent intent = new Intent(Dragporte.this, Victory.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
