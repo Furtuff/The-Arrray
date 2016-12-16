2099283dd023cabd637c4ddf13e1a44cc917687c
clément mezerette <clement.mezerette@gmail.com>
2016-12-16 11:38:38 +0100

    V10

--- app/src/main/java/fr/wildcodeschool/team/thearray/Victory.java
+++ app/src/main/java/fr/wildcodeschool/team/thearray/Victory.java
@@ -6,6 +6,8 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Victory extends AppCompatActivity {
 
@@ -20,5 +22,17 @@ public class Victory extends AppCompatActivity {
 MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.hallelluja);
         mediaPlayer.start();
         getIntent().getLongExtra("score",0);
+
+        ImageView img = (ImageView)findViewById(R.id.imageView2);
+        img.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View V){
+                {
+                    Intent intent = new Intent(Victory.this, Scores.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
