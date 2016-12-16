d3e885d07e8ebc37efa1508343c59d1fab496fec
tuffery <furtuff@gmail.com>
2016-12-16 11:27:13 +0100

    hotfix4

--- app/src/main/java/fr/wildcodeschool/team/thearray/Victory.java
+++ app/src/main/java/fr/wildcodeschool/team/thearray/Victory.java
@@ -1,9 +1,11 @@
 package fr.wildcodeschool.team.thearray;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Victory extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class Victory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_victory);
+        TextView textView = (TextView)findViewById(R.id.text) ;
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"verdana.ttf");
+        textView.setTypeface(typeface);
+
 MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.hallelluja);
         mediaPlayer.start();
         getIntent().getLongExtra("score",0);
